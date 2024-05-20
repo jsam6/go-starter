5d232ad37dac14a681af1fdc2e1df776cb257902
sam chong <cjsam6@yahoo.com>
2024-05-20 23:52:44 +0800

    😂 understanding go

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/jsam6/go-orders-api/modules/user"
 	"github.com/jsam6/go-orders-api/config"
+	"github.com/jsam6/go-orders-api/modules/user"
 )
 
 func main() {
+	
+	helloWorld := "helloworld"
+	var pointerToHelloWorld *string
+	pointerToHelloWorld = &helloWorld
+	fmt.Println(helloWorld)
+	fmt.Println(&helloWorld)
+	fmt.Println()
+	fmt.Println(pointerToHelloWorld)
+	fmt.Println(&pointerToHelloWorld)
+	fmt.Println(*pointerToHelloWorld)
+	fmt.Println()
+	fmt.Println(*&pointerToHelloWorld)
+	fmt.Println(&*pointerToHelloWorld)
+
 	config.Connect()
 
 	router := chi.NewRouter()
